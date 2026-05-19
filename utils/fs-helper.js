e8b0b7e8ae512d65e2e2f21132d8d9b000906041
Daniel Santos <daniel@solucx.com.br>
2026-05-19 17:59:41 -0300

    tudo funcionando !

--- utils/fs-helper.js
+++ utils/fs-helper.js
@@ -1,5 +1,28 @@
 import { openAssetsSync, readSync, statAssetsSync, O_RDONLY, closeSync } from "@zos/fs";
 
+function decodeUtf8(bytes) {
+  let str = "";
+  let i = 0;
+  while (i < bytes.length) {
+    const b = bytes[i];
+    if (b < 0x80) {
+      str += String.fromCharCode(b);
+      i += 1;
+    } else if ((b & 0xe0) === 0xc0) {
+      str += String.fromCharCode(((b & 0x1f) << 6) | (bytes[i + 1] & 0x3f));
+      i += 2;
+    } else if ((b & 0xf0) === 0xe0) {
+      str += String.fromCharCode(
+        ((b & 0x0f) << 12) | ((bytes[i + 1] & 0x3f) << 6) | (bytes[i + 2] & 0x3f)
+      );
+      i += 3;
+    } else {
+      i += 1;
+    }
+  }
+  return str;
+}
+
 function readText(path) {
   try {
     const stat = statAssetsSync({ path });
@@ -10,7 +33,7 @@ function readText(path) {
     closeSync({ fd });
     return decodeUtf8(new Uint8Array(buf));
   } catch (e) {
-    console.error(`Error reading ${path}:`, e);
+    console.error(`[fs-helper] Error reading "${path}":`, e && e.message ? e.message : e);
     return null;
   }
 }
